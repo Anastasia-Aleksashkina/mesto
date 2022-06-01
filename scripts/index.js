54e7537a4890f359d49331bd344c948adc9d6c4c
Anastasia Aleksashkina <aleksashkina.nastya@yandex.ru>
2022-06-02 00:51:41 +0300

    refactor: новый popup добавления фото, лайки, добавление карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,83 @@
-const editButton = document.querySelector(".profile__button-edit");
-const popup = document.querySelector(".popup");
-const popupClose = document.querySelector(".popup__button-close");
-const formElement = document.querySelector(".popup__form");
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+const popupCloseUser = document.querySelector(".popup__button-close");
+const popupCloseCard = document.querySelector(".popup__button-close_new-card");
+const editButtonUser = document.querySelector(".profile__button-edit");
+const editButtonCard = document.querySelector(".profile__button-edd");
+const formElementUser = document.querySelector(".popup__form");
+const formElementCard = document.querySelector(".popup__form_new-card");
 const nameElement = document.querySelector(".profile__user-name");
 const aboutElement = document.querySelector(".profile__user-about");
 const nameFieldElement = document.querySelector(".popup__string-name");
 const aboutFieldElement = document.querySelector(".popup__string-about");
+const nameElementCity = document.querySelector(".element__city");
+const aboutElementLink = document.querySelector(".element__image");
+const nameFieldElementCity = document.querySelector(".popup__string-name_new-card");
+const aboutFieldElementLink = document.querySelector(".popup__string-about_new-card");
+const popupUser = document.querySelector(".popup");
+const popupNewCard = document.querySelector(".popup_new-card");
+const openPopup = (popupElement) => {
+  popupElement.classList.add("popup_opened");
+};
+const closePopup = (popupElement) => {
+  popupElement.classList.remove("popup_opened");
+};
+const cardContainer = document.querySelector(".elements");
 
-function openPopup() {
-  popup.classList.add("popup_opened");
-  nameFieldElement.value = nameElement.textContent;
-  aboutFieldElement.value = aboutElement.textContent;
+nameFieldElement.value = nameElement.textContent;
+aboutFieldElement.value = aboutElement.textContent;
+function addNewCard(element) {
+  const cardTemplate = document.querySelector("#element-template").content;
+  const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
+  cardElement.querySelector(".element__city").textContent = element.name;
+  cardElement.querySelector(".element__image").src = element.link;
+  cardElement
+    .querySelector(".element__like")
+    .addEventListener("click", function (e) {
+      e.target.classList.toggle("element__like_active");
+    });
+  cardContainer.prepend(cardElement);
 }
-
-function closePopup() {
-  popup.classList.remove("popup_opened");
-}
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
+initialCards.forEach(addNewCard);
+function formSubmitHandlerUser(e) {
+  e.preventDefault();
   nameElement.textContent = nameFieldElement.value;
   aboutElement.textContent = aboutFieldElement.value;
-  closePopup()
+  closePopup(popupUser);
 }
-
-editButton.addEventListener("click", openPopup);
-popupClose.addEventListener("click", closePopup);
-formElement.addEventListener("submit", formSubmitHandler);
+function formSubmitHandlerCard(e) {
+  e.preventDefault();
+  nameElementCity.textContent = nameFieldElementCity.value;
+  aboutElementLink.textContent = aboutFieldElementLink.value;
+  closePopup(popupUser);
+}
+editButtonUser.addEventListener("click", () => openPopup(popupUser));
+editButtonCard.addEventListener("click", () => openPopup(popupNewCard));
+popupCloseUser.addEventListener("click", () => closePopup(popupUser));
+popupCloseCard.addEventListener("click", () => closePopup(popupNewCard));
+formElementUser.addEventListener("submit", formSubmitHandlerUser);
+formElementCard.addEventListener("submit", formSubmitHandlerCard);
