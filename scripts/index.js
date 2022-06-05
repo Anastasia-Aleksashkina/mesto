07fec8657ffba7fbb38984da58335465584fb453
Anastasia Aleksashkina <aleksashkina.nastya@yandex.ru>
2022-06-05 10:03:31 +0300

    refactor: открытие попапа с картинкой

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 const popupUser = document.querySelector(".popup");
+const popupNewCard = document.querySelector(".popup_new-card");
+const popupNewImage = document.querySelector(".popup_image");
 const formElementUser = document.querySelector(".popup__form");
 const formElementCard = document.querySelector(".popup__form_new-card");
-// const formElementImage = document.querySelector(".popup__form_image");
+const formElementImage = document.querySelector(".popup__form_image");
 const openPopup = (popupElement) => {
   popupElement.classList.add("popup_opened");
 };
@@ -10,6 +12,7 @@ const closePopup = (popupElement) => {
 };
 const popupCloseUser = document.querySelector(".popup__button-close");
 const popupCloseCard = document.querySelector(".popup__button-close_new-card");
+const popupCloseImage = document.querySelector(".popup__button-close_image");
 const nameFieldElement = formElementUser.querySelector(".popup__string-name");
 const aboutFieldElement = formElementUser.querySelector(".popup__string-about");
 const cityFieldElement = formElementCard.querySelector(
@@ -24,8 +27,7 @@ const editImageCard = document.querySelector(".popup__image");
 const nameElement = document.querySelector(".profile__user-name");
 const aboutElement = document.querySelector(".profile__user-about");
 const cardContainer = document.querySelector(".elements");
-const popupNewCard = document.querySelector(".popup_new-card");
-const getCardByElement = e => e.currentTarget.closest(".element");
+const getCardByElement = (e) => e.currentTarget.closest(".element");
 
 nameFieldElement.value = nameElement.textContent;
 aboutFieldElement.value = aboutElement.textContent;
@@ -35,38 +37,49 @@ const addCard = (element) => {
   const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
   cardElement.querySelector(".element__city").textContent = element.name;
   cardElement.querySelector(".element__image").src = element.link;
-  cardElement.querySelector(".element__like").addEventListener("click", e => {
+  cardElement.querySelector(".element__like").addEventListener("click", (e) => {
     e.target.classList.toggle("element__like_active");
   });
-  cardElement.querySelector(".element__delete").addEventListener("click", e => {
-    const element = getCardByElement(e);
-    element.remove();
-  });
+  cardElement
+    .querySelector(".element__delete")
+    .addEventListener("click", (e) => {
+      const element = getCardByElement(e);
+      element.remove();
+    });
   cardContainer.prepend(cardElement);
 };
 initialCards.forEach(addCard);
 
-const handlerProfileSubmit = e => {
+const handlerProfileSubmit = (e) => {
   e.preventDefault();
   nameElement.textContent = nameFieldElement.value;
   aboutElement.textContent = aboutFieldElement.value;
   closePopup(popupUser);
 };
 
-const handlerCardSubmit = e => {
+const handlerCardSubmit = (e) => {
   e.preventDefault();
   const card = {
     name: cityFieldElement.value,
-    link: linktFieldElement.value
-  }
+    link: linktFieldElement.value,
+  };
   addCard(card);
   closePopup(popupNewCard);
 };
 
+//попап с картинкой
+const openImagePopup = (e) => {
+  const imageElement = getCardByElement(e);
+  openPopup();
+  linktFieldElement.value = imageElement.querySelector(".popup__image").src;
+  cityFieldElement.value = imageElement.querySelector(".popup__caption").textContent;
+};
+popupNewImage.addEventListener("click", openImagePopup);
+
 editButtonUser.addEventListener("click", () => openPopup(popupUser));
 editButtonCard.addEventListener("click", () => openPopup(popupNewCard));
-// editImageCard.addEventListener("click", () => openPopup(popupImageCard));
 popupCloseUser.addEventListener("click", () => closePopup(popupUser));
 popupCloseCard.addEventListener("click", () => closePopup(popupNewCard));
+popupCloseImage.addEventListener("click", () => closePopup(popupNewImage));
 formElementUser.addEventListener("submit", handlerProfileSubmit);
 formElementCard.addEventListener("submit", handlerCardSubmit);
